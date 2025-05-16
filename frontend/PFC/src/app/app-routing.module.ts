eacec3cce359adfd4b0d77af6cd5f0b0f010008d
davidnoya <dnoyav23@fpcoruna.afundacion.org>
2025-05-16 09:31:49 +0200

    Optimización de la aplicación y primera página de bienvenida.

--- frontend/PFC/src/app/app-routing.module.ts
+++ frontend/PFC/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidaComponent } from './traslado-cuentas/component/bienvenida/bienvenida.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/bienvenida', pathMatch: 'full'},
+  {path: 'bienvenida', component: BienvenidaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/PFC/src/app/app.module.ts
+++ frontend/PFC/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PieComponent } from './shared/pie/pie.component';
+import { BienvenidaComponent } from './traslado-cuentas/component/bienvenida/bienvenida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieComponent,
+    BienvenidaComponent
   ],
   imports: [
     BrowserModule,
