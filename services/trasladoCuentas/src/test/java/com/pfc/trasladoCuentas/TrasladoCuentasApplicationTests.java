d93c58f7c0191b39a8c5ad15f54d8ffbda6ffccb
davidnoya <dnoyav23@fpcoruna.afundacion.org>
2025-05-29 20:01:01 +0200

    Creación de proyecto Maven es Spring Boot

---
+++ services/trasladoCuentas/src/test/java/com/pfc/trasladoCuentas/TrasladoCuentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfc.trasladoCuentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrasladoCuentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
