78c97f2efa70a4a058c8f38d25fa1aed83546193
davidnoya <dnoyav23@fpcoruna.afundacion.org>
2025-06-06 18:04:59 +0200

    Funcionalidad de login COMPLETA

--- frontend/PFC/src/app/traslado-cuentas/component/login/login.component.ts
+++ frontend/PFC/src/app/traslado-cuentas/component/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { LoginService } from '../../service/login.service';
 
 @Component({
@@ -13,23 +13,32 @@ export class LoginComponent implements OnInit {
   pin: string = '';
   errorMessage: string = '';
 
-  constructor(private route: ActivatedRoute, private loginService: LoginService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private loginService: LoginService
+  ) { }
 
   ngOnInit(): void {
-    this.texto = this.route.snapshot.data['texto']
+    this.texto = this.route.snapshot.data['texto'];
   }
 
   onSubmit(): void {
-    this.loginService.validarCredenciales(this.dni, this.pin).subscribe({
+    this.errorMessage = '';
+    this.loginService.login(this.dni, this.pin).subscribe({
       next: (response) => {
-        if (response.valido) {
-          alert('Credenciales válidas');
+        if (response === 'Login correcto') {
+          this.router.navigate(['/banca-electronica']);
         } else {
           this.errorMessage = 'Credenciales inválidas';
         }
       },
-      error: (err) => {
-        this.errorMessage = 'Error al conectar con el servidor'; 
+      error: (error) => {
+        if (error.status = 401) {
+          this.errorMessage = 'Credenciales inválidas';
+        } else {
+          this.errorMessage = 'Error al conectar con el servidor';
+        }
       }
     });
   }
